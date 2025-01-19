5e667c2442c5450627820dabd52c907203cbca1c
dikshantkanela <dikshant7156@gmail.com>
2025-01-19 17:10:27 +0530

    function that returns another function

--- FUNCTIONS/functionThatReturnsAFunc.js
+++ FUNCTIONS/functionThatReturnsAFunc.js
@@ -1,10 +1,9 @@
-function tenTimes(funky){   //Parameter is a function
-    return funky()*10;
- }
-function rollDice (){
-     let roll= Math.floor(Math.random()*6)+1
-     return roll;
- }
- 
- const result =tenTimes(rollDice)  ; 
- console.log(result);
+const createGreeting = (greetStr)=>{ //THIS IS A STRING!
+    return (name)=>{ //returned function
+        return `${greetStr} ${name}`;
+    }   
+}
+
+const greeting = createGreeting("CONGO");
+const final = greeting("Dikshant");
+console.log(final);
--- FUNCTIONS/highOrderFunc.js
+++ FUNCTIONS/highOrderFunc.js
@@ -11,3 +11,4 @@ function rollDice(){
 tenTimes(rollDice)  ;
 
 
+
