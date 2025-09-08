1bc49dd52a4d769c95dbff87bae98907a315fa4a
dikshantkanela <dikshant7156@gmail.com>
2025-09-08 19:24:04 +0530

    shadowing

--- SHADOWING/shadowing.js
+++ SHADOWING/shadowing.js
@@ -1,3 +1,13 @@
+/* 
+In JavaScript, variable shadowing occurs when a variable declared in an inner scope has the same name as a variable in an outer scope.
+The inner variable “shadows” (hides) the outer one within that inner scope.
+
+Key Points:
+Shadowing happens in nested scopes.
+The inner variable takes precedence over the outer variable within its scope.
+It does not affect the outer variable outside the inner scope.
+*/
+
 let x = 10; // global scope
 
 function outer() {
